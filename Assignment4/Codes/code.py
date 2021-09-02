6b79ca7260e410e0b3243db87c2d675f499c7cc0
Sujal Akarniya <75010367+sujal100@users.noreply.github.com>
2021-09-02 21:24:39 +0530

    Update code.py

--- Assignment4/Codes/code.py
+++ Assignment4/Codes/code.py
@@ -3,31 +3,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 from coeffs import *
 
-
 # This function use least square to find minimum between lines
-def distance_from_two_lines(e1, e2, r1, r2):
-    # e1, e2 = Direction vector
-    # r1, r2 = Point where the line passes through
-
-    # Find the unit vector perpendicular to both lines
-    A = np.concatenate((e1,e2)).reshape(2,3).T
-    inverse = np.linalg.inv(A.T @ A)
-    B = r2 - r1
-    x = (inverse @ A.T) @ B
+def distance_between_skew_lines(A,B):
     
+    inverse = np.linalg.inv(A.T @ A)
+    x = (inverse @ A.T) @ B
+
     # Calculate distance
     d = np.linalg.norm((A @ x) - B)
     return d
+
 
 # Plane points
 A1 = np.array([-1, -1, -1])
 A2 = np.array([3, 5, 7])
 m1 = np.array([7, -6, 1])
 m2 = np.array([1, -2, 1])
+# m1, m2 = Direction vector
+# A1, A2 = Point where the line passes through
 
-print("Shortest distance between given skew lines :",distance_from_two_lines(m1, m2, A1, A2))
-# Output 
-# Shortest distance between given skew lines : 10.77032961426901
+A = np.concatenate((m1, m2)).reshape(2, 3).T
+B = A1-A2
+
+print("Shortest distance between given skew lines :",
+      distance_between_skew_lines(A, B))
+
+print("Verifying using inbuilt function :", np.linalg.norm(
+    (A @ np.linalg.lstsq(A, B, rcond=None)[0]) - B))
+
+'''
+# Output
+Shortest distance between given skew lines : 10.77032961426901
+Verifying using inbuilt function : 10.770329614269007
+'''
 
 # creating x,y for 3D plotting
 xx, yy = np.meshgrid(range(10), range(10))
@@ -41,8 +49,10 @@ x2_dist_skew = line_dir_pt(m2, A2, k1, k2)
 x1_dist_skew = line_dir_pt(m1, A1, k1, k2)
 
 # Plotting all lines
-plt.plot(x1_dist_skew[0, :], x1_dist_skew[1, :], x1_dist_skew[2, :], label='$L_1$')
-plt.plot(x2_dist_skew[0, :], x2_dist_skew[1, :], x2_dist_skew[2, :], label='$L_2$')
+plt.plot(x1_dist_skew[0, :], x1_dist_skew[1, :],
+         x1_dist_skew[2, :], label='$L_1$')
+plt.plot(x2_dist_skew[0, :], x2_dist_skew[1, :],
+         x2_dist_skew[2, :], label='$L_2$')
 # plotting points
 ax.scatter(A1[0], A1[1], A1[2], 'o')
 ax.scatter(A2[0], A2[1], A2[2], 'o')
